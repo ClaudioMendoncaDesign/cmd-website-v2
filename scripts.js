08b4bfc3adeb64bfb4efd241466f0c9d4bcf385d
Claudio Mendonca <claudioccm@gmail.com>
2016-04-05 08:12:29 -0400

    rebuild

--- scripts.js
+++ scripts.js
@@ -1,18 +1,35 @@
 $(document).ready(function() {
 
+// Main Menu
+$('.js-main-menu').click(function() {
+    var sidebar = $('.sidebar');
+
+    if (sidebar.hasClass('js-active')) {
+        sidebar.removeClass('js-active');    
+        $(this).removeClass('js-active');    
+    } else {
+        sidebar.addClass('js-active');    
+        $(this).addClass('js-active');    
+    }
+});
+
+
+
 // Slider Config (Sticky)
-    $('.slider').slick({
-        arrows: true,
-        draggable: false,
-        swipeToSlide: true,
-        responsive: [
-            {
-                breakpoint: 800,
-                settings: {
-                    draggable: true,
-                }
+$('.slider').slick({
+    arrows: true,
+    draggable: false,
+    swipeToSlide: true,
+    responsive: [
+        {
+            breakpoint: 800,
+            settings: {
+                draggable: true,
             }
-        ]
-    });
+        }
+    ]
+});
+
+
 
 });
