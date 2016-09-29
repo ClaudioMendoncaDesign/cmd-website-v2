51a2000973901a3e518dc703aee5694b93fd2b20
Claudio Mendonca <claudioccm@gmail.com>
2016-09-29 14:37:11 -0400

    design update for iodc

--- scripts.js
+++ scripts.js
@@ -30,6 +30,41 @@ $('.slider').slick({
     ]
 });
 
+$('.intro-slider').slick({
+    arrows: false,
+    draggable: false,
+    swipeToSlide: true,
+    autoplay: true,
+    fade: true,
+    infinite: true,
+    speed: 5000,
+    autoplaySpeed: 1000,
+    responsive: [
+        {
+            breakpoint: 800,
+            settings: {
+                draggable: true,
+            }
+        }
+    ]
+});
+
+// $(function() {
+
+//   var $quote = $(".quote__text");
+  
+//   // var $numWords = $quote.text().split(" ").length;
+//   var $numWords = $quote.length;
+//   console.log($numWords);
+  
+//   if (($numWords >= 1) && ($numWords < 100)) {
+//       $quote.addClass('quote--short');
+//   }
+//   else {
+//       $quote.addClass('quote--long');
+//   }    
+
+// });
 
 
 });
